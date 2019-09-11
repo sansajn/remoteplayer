1d417f85ffd564de6960849e06a741765fe43a61
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2019-09-11 16:56:03 +0200

    partial work

--- arplay/app/src/main/java/remoteplayer/arplay/PlaylistAdapter.kt
+++ arplay/app/src/main/java/remoteplayer/arplay/PlaylistAdapter.kt
@@ -1,11 +1,14 @@
 package remoteplayer.arplay
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.simple_playlist_item.view.*
 
 data class PlaylistItem(
 	val title: String,
@@ -58,4 +61,31 @@ class PlaylistListAdapter(val context: Context, val items: List<PlaylistItem>) :
 		val title: TextView
 		val subtitle: TextView
 	}
+}
+
+class PlaylistRecyclerAdapter(var c: Context, var list: List<PlaylistItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+		var v = LayoutInflater.from(c).inflate(R.layout.simple_playlist_item, parent, false)
+		return Item(v)
+	}
+
+	override fun getItemCount(): Int {
+		return list.size
+	}
+
+	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+		(holder as Item).bindData(list[position])
+	}
+
+	class Item(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		fun bindData(item: PlaylistItem) {
+			itemView.title.text = item.title
+			itemView.subtitle.text = item.artist
+			itemView.setOnClickListener {
+				Toast.makeText(itemView.context, "item $item clicked", Toast.LENGTH_LONG).show()
+			}
+		}
+	}
+
 }
